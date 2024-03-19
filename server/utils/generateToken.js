e6b19fdecea6e1e79eb013e8e3b1eab0bf97abf9
Rohit Raj <rohit.rajcs2020@indoreinstitute.com>
2024-03-20 02:06:38 +0530

    first part completed

---
+++ server/utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenAndSetCookie = ( userId, res ) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:"15d",
+    } );
+    
+    // Set cookie in response header
+    res.cookie("jwt", token, {
+        maxAge:15*24*60*60*1000,
+        httpOnly:true, // prevent access from client side script or  XSS attacks  
+        sameSite:"strict", // CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development", 
+
+    });
+}
+
+export default generateTokenAndSetCookie;
